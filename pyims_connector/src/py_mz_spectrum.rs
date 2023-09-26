c19abd00a932e5a01f14c0af14541ef7ff518b46
theGreatHerrLebert <davidteschner@googlemail.com>
2023-09-26 10:17:27 +0200

    fixing...

--- pyims_connector/src/py_mz_spectrum.rs
+++ pyims_connector/src/py_mz_spectrum.rs
@@ -172,6 +172,7 @@ impl PyTimsSpectrum {
         self.inner.inv_mobility
     }
 
+    #[getter]
     pub fn index(&self, py: Python) -> Py<PyArray1<i32>> {
         self.inner.spectrum.index.clone().into_pyarray(py).to_owned()
     }
