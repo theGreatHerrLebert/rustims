6a21d40f375fff85f3ec9875be199fef2c6c546b
theGreatHerrLebert <davidteschner@googlemail.com>
2023-09-27 15:19:29 +0200

    updating impl

--- imsjl_connector/src/frame.rs
+++ imsjl_connector/src/frame.rs
@@ -1,3 +1,4 @@
+use std::slice;
 use mscore::TimsFrame;
 
 #[repr(C)]
@@ -24,24 +25,41 @@ pub struct CTimsFrame {
 
 #[no_mangle]
 pub extern "C" fn convert_to_ctims_frame(frame: TimsFrame) -> CTimsFrame {
+    // Clone the vectors and convert them to boxed slices
+    let scan_boxed = frame.scan.clone().into_boxed_slice();
+    let inv_mobility_boxed = frame.ims_frame.inv_mobility.clone().into_boxed_slice();
+    let tof_boxed = frame.tof.clone().into_boxed_slice();
+    let mz_boxed = frame.ims_frame.mz.clone().into_boxed_slice();
+    let intensity_boxed = frame.ims_frame.intensity.clone().into_boxed_slice();
+
     CTimsFrame {
         frame_id: frame.frame_id,
         ms_type: frame.ms_type.ms_type_numeric(),
         retention_time: frame.ims_frame.retention_time,
 
-        scan: frame.scan.as_ptr(),
+        scan: Box::into_raw(scan_boxed) as *const i32,
         scan_size: frame.scan.len(),
 
-        inv_mobility: frame.ims_frame.inv_mobility.as_ptr(),
+        inv_mobility: Box::into_raw(inv_mobility_boxed) as *const f64,
         inv_mobility_size: frame.ims_frame.inv_mobility.len(),
 
-        tof: frame.tof.as_ptr(),
+        tof: Box::into_raw(tof_boxed) as *const i32,
         tof_size: frame.tof.len(),
 
-        mz: frame.ims_frame.mz.as_ptr(),
+        mz: Box::into_raw(mz_boxed) as *const f64,
         mz_size: frame.ims_frame.mz.len(),
 
-        intensity: frame.ims_frame.intensity.as_ptr(),
+        intensity: Box::into_raw(intensity_boxed) as *const f64,
         intensity_size: frame.ims_frame.intensity.len(),
+    }
+}
+#[no_mangle]
+pub extern "C" fn free_ctims_frame_data(frame: CTimsFrame) {
+    unsafe {
+        let _ = Box::from_raw(slice::from_raw_parts_mut(frame.scan as *mut i32, frame.scan_size));
+        let _ = Box::from_raw(slice::from_raw_parts_mut(frame.inv_mobility as *mut f64, frame.inv_mobility_size));
+        let _ = Box::from_raw(slice::from_raw_parts_mut(frame.tof as *mut i32, frame.tof_size));
+        let _ = Box::from_raw(slice::from_raw_parts_mut(frame.mz as *mut f64, frame.mz_size));
+        let _ = Box::from_raw(slice::from_raw_parts_mut(frame.intensity as *mut f64, frame.intensity_size));
     }
 }
