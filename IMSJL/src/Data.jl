42a66e1f7c156c574babb136b28cab86a340342e
theGreatHerrLebert <davidteschner@googlemail.com>
2023-09-27 20:37:57 +0200

    fixing

--- IMSJL/src/Data.jl
+++ IMSJL/src/Data.jl
@@ -16,7 +16,7 @@ struct TimsFrame
 end
 
 function show(io::IO, frame::TimsFrame)
-    num_peaks = length(frame.mz)  # or whichever array represents peaks
+    num_peaks = length(frame.mz)
     print(io, "TimsFrame(frame_id=$(frame.frame_id), ms_type=$(frame.ms_type_numeric), num_peaks=$num_peaks)")
 end
 
