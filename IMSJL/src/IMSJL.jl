e01978c348cb20a42f9caae4caedc6a00116eeb5
theGreatHerrLebert <davidteschner@googlemail.com>
2023-09-27 22:19:42 +0200

    testing

--- IMSJL/src/IMSJL.jl
+++ IMSJL/src/IMSJL.jl
@@ -4,6 +4,6 @@ include("Data.jl")
 include("RustCAPI.jl")
 include("JuliaDataHandle.jl")
 
-export Data, RustCAPI, JuliaDataHandle
+export Data, RustCAPI, JuliaDataHandle.TimsDataHandle, JuliaDataHandle.get_tims_frame
 
 end
