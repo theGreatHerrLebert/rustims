54443c1bf7c4320e86fe744864d74c371803919c
theGreatHerrLebert <davidteschner@googlemail.com>
2023-09-28 10:28:58 +0200

    added meta data return

--- IMSJL/src/DataHandle.jl
+++ IMSJL/src/DataHandle.jl
@@ -1,10 +1,12 @@
 module DataHandle
 
+using DataFrames, FilePathsBase
 using IMSJL.Data, IMSJL.RustCAPI
 
 struct TimsDataHandle
     data_path::String
     bruker_binary_path::String
+    frame_meta_data::DataFrame
     handle::Ptr{Cvoid}  # or appropriate type for the handle
 
     # Constructor that only requires the data_path
@@ -16,8 +18,11 @@ struct TimsDataHandle
         # Acquire the actual handle from the C-exposed Rust object
         handle = RustCAPI.TimsDataHandle_new(data_path, bruker_binary_path)
 
+        # Acquire the frame meta data
+        frame_meta_data = get_frame_meta_data(data_path)
+
         # Construct the instance
-        new(data_path, bruker_binary_path, handle)
+        new(data_path, bruker_binary_path, frame_meta_data, handle)
     end
 end
 
@@ -32,6 +37,11 @@ end
 function get_tims_frame(handle::TimsDataHandle, frame_id::Number)::TimsFrame
     ctims_frame = RustCAPI.TimsDataHandle_get_frame(handle.handle, Int32(frame_id))
     return RustCAPI.ctims_frame_to_julia_tims_frame(ctims_frame)
+end
+
+function get_frame_meta_data(ds_path::String)::DataFrame
+    db = SQLite.DB(string(Path(ds_path), Path("/analysis.tdf")))
+    return DataFrame(DBInterface.execute(db, "SELECT * FROM Frames"))
 end
 
 export TimsDataHandle, get_tims_frame
