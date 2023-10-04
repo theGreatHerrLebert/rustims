ae994776655e5d4ea1614f2df8ee3be707763aee
theGreatHerrLebert <davidteschner@googlemail.com>
2023-10-04 15:30:38 +0200

    fix binning of timsFrame

--- mscore/src/tims_frame.rs
+++ mscore/src/tims_frame.rs
@@ -200,31 +200,35 @@ impl TimsFrame {
         let factor = (10.0f64).powi(resolution);
 
         // Using a tuple of (scan, mz_bin) as a key
-        // Value will store sum of intensities and sum of tofs and their count for averaging
-        let mut bin_map: BTreeMap<(i32, i32), (f64, f64, i32)> = BTreeMap::new();
+        // Value will store sum of intensities, sum of tofs, sum of mobilities and their count for averaging
+        let mut bin_map: BTreeMap<(i32, i32), (f64, f64, f64, i32)> = BTreeMap::new();
 
         for i in 0..self.ims_frame.mz.len() {
             let rounded_mz = (self.ims_frame.mz[i] * factor).round() as i32;
             let scan_val = self.scan[i];
-            let intensity_val = self.ims_frame.intensity[i];
+            let intensity_val = self.ims_frame.intensity[i] as f64;
             let tof_val = self.tof[i] as f64;
+            let mobility_val = self.ims_frame.mobility[i] as f64;
 
-            let entry = bin_map.entry((scan_val, rounded_mz)).or_insert((0.0, 0.0, 0));
+            let entry = bin_map.entry((scan_val, rounded_mz)).or_insert((0.0, 0.0, 0.0, 0));
             entry.0 += intensity_val;
             entry.1 += tof_val;
-            entry.2 += 1;
+            entry.2 += mobility_val;
+            entry.3 += 1;
         }
 
         let mut new_mz = Vec::with_capacity(bin_map.len());
         let mut new_scan = Vec::with_capacity(bin_map.len());
         let mut new_intensity = Vec::with_capacity(bin_map.len());
         let mut new_tof = Vec::with_capacity(bin_map.len());
+        let mut new_mobility = Vec::with_capacity(bin_map.len());
 
-        for ((scan, mz_bin), (intensity_sum, tof_sum, count)) in bin_map {
+        for ((scan, mz_bin), (intensity_sum, tof_sum, mobility_sum, count)) in bin_map {
             new_mz.push(mz_bin as f64 / factor);
             new_scan.push(scan);
-            new_intensity.push(intensity_sum as f64);
+            new_intensity.push(intensity_sum);
             new_tof.push((tof_sum / count as f64) as i32);
+            new_mobility.push(mobility_sum / count as f64);
         }
 
         TimsFrame {
@@ -234,10 +238,10 @@ impl TimsFrame {
             tof: new_tof,
             ims_frame: ImsFrame {
                 retention_time: self.ims_frame.retention_time,
-                mobility: self.ims_frame.mobility.clone(),
+                mobility: new_mobility,
                 mz: new_mz,
                 intensity: new_intensity,
-            }
+            },
         }
     }
 
