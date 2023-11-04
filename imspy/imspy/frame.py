efe17e1c6fa65cf7855e88dfbd2c1c37d9a79f39
theGreatHerrLebert <davidteschner@googlemail.com>
2023-11-04 12:38:13 +0100

    need to refactor project name, pyims unfortunately already taken.

---
+++ imspy/imspy/frame.py
@@ -0,0 +1,391 @@
+import pandas as pd
+
+from typing import List
+from numpy.typing import NDArray
+
+from tensorflow import sparse as sp
+
+import numpy as np
+import pyims_connector as pims
+from pyims.spectrum import MzSpectrum, TimsSpectrum, IndexedMzSpectrum
+
+from pyims.utilities import re_index_indices
+
+
+class TimsFrame:
+    def __init__(self, frame_id: int, ms_type: int, retention_time: float, scan: NDArray[np.int32],
+                 mobility: NDArray[np.float64], tof: NDArray[np.int32],
+                 mz: NDArray[np.float64], intensity: NDArray[np.float64]):
+        """TimsFrame class.
+
+        Args:
+            frame_id (int): Frame ID.
+            ms_type (int): MS type.
+            retention_time (float): Retention time.
+            scan (NDArray[np.int32]): Scan.
+            mobility (NDArray[np.float64]): Inverse mobility.
+            tof (NDArray[np.int32]): Time of flight.
+            mz (NDArray[np.float64]): m/z.
+            intensity (NDArray[np.float64]): Intensity.
+
+        Raises:
+            AssertionError: If the length of the scan, mobility, tof, mz and intensity arrays are not equal.
+        """
+
+        assert len(scan) == len(mobility) == len(tof) == len(mz) == len(intensity), \
+            "The length of the scan, mobility, tof, mz and intensity arrays must be equal."
+
+        self.__frame_ptr = pims.PyTimsFrame(frame_id, ms_type, retention_time, scan, mobility, tof, mz, intensity)
+
+    @classmethod
+    def from_py_tims_frame(cls, frame: pims.PyTimsFrame):
+        """Create a TimsFrame from a PyTimsFrame.
+
+        Args:
+            frame (pims.PyTimsFrame): PyTimsFrame to create the TimsFrame from.
+
+        Returns:
+            TimsFrame: TimsFrame created from the PyTimsFrame.
+        """
+        instance = cls.__new__(cls)
+        instance.__frame_ptr = frame
+        return instance
+
+    @property
+    def frame_id(self) -> int:
+        """Frame ID.
+
+        Returns:
+            int: Frame ID.
+        """
+        return self.__frame_ptr.frame_id
+
+    @property
+    def ms_type(self) -> str:
+        """MS type.
+
+        Returns:
+            int: MS type.
+        """
+        return self.__frame_ptr.ms_type_as_string
+
+    @property
+    def retention_time(self) -> float:
+        """Retention time.
+
+        Returns:
+            float: Retention time.
+        """
+        return self.__frame_ptr.retention_time
+
+    @property
+    def scan(self) -> NDArray[np.int32]:
+        """Scan.
+
+        Returns:
+            NDArray[np.int32]: Scan.
+        """
+        return self.__frame_ptr.scan
+
+    @property
+    def mobility(self) -> NDArray[np.float64]:
+        """Inverse mobility.
+
+        Returns:
+            NDArray[np.float64]: Inverse mobility.
+        """
+        return self.__frame_ptr.mobility
+
+    @property
+    def tof(self) -> NDArray[np.int32]:
+        """Time of flight.
+
+        Returns:
+            NDArray[np.int32]: Time of flight.
+        """
+        return self.__frame_ptr.tof
+
+    @property
+    def mz(self) -> NDArray[np.float64]:
+        """m/z.
+
+        Returns:
+            NDArray[np.float64]: m/z.
+        """
+        return self.__frame_ptr.mz
+
+    @property
+    def intensity(self) -> NDArray[np.float64]:
+        """Intensity.
+
+        Returns:
+            NDArray[np.float64]: Intensity.
+        """
+        return self.__frame_ptr.intensity
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """ Data as a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: Data.
+        """
+
+        return pd.DataFrame({
+            'frame': np.repeat(self.frame_id, len(self.scan)),
+            'retention_time': np.repeat(self.retention_time, len(self.scan)),
+            'scan': self.scan,
+            'mobility': self.mobility,
+            'tof': self.tof,
+            'mz': self.mz,
+            'intensity': self.intensity})
+
+    def filter(self,
+               mz_min: float = 0.0,
+               mz_max: float = 2000.0,
+               scan_min: int = 0,
+               scan_max: int = 1000,
+               mobility_min: float = 0.0,
+               mobility_max: float = 2.0,
+               intensity_min: float = 0.0,
+               intensity_max: float = 1e9,
+               ) -> 'TimsFrame':
+        """Filter the frame for a given m/z range, scan range and intensity range.
+
+        Args:
+            mz_min (float): Minimum m/z value.
+            mz_max (float): Maximum m/z value.
+            scan_min (int, optional): Minimum scan value. Defaults to 0.
+            scan_max (int, optional): Maximum scan value. Defaults to 1000.
+            mobility_min (float, optional): Minimum inverse mobility value. Defaults to 0.0.
+            mobility_max (float, optional): Maximum inverse mobility value. Defaults to 2.0.
+            intensity_min (float, optional): Minimum intensity value. Defaults to 0.0.
+            intensity_max (float, optional): Maximum intensity value. Defaults to 1e9.
+
+        Returns:
+            TimsFrame: Filtered frame.
+        """
+
+        return TimsFrame.from_py_tims_frame(self.__frame_ptr.filter_ranged(mz_min, mz_max, scan_min, scan_max, mobility_min, mobility_max,
+                                                                           intensity_min, intensity_max))
+
+    def to_indexed_mz_spectrum(self) -> 'IndexedMzSpectrum':
+        """Convert the frame to an IndexedMzSpectrum.
+
+        Returns:
+            IndexedMzSpectrum: IndexedMzSpectrum.
+        """
+        return IndexedMzSpectrum.from_py_indexed_mz_spectrum(self.__frame_ptr.to_indexed_mz_spectrum())
+
+    def to_resolution(self, resolution: int) -> 'TimsFrame':
+        """Convert the frame to a given resolution.
+
+        Args:
+            resolution (int): Resolution.
+
+        Returns:
+            TimsFrame: Frame with the given resolution.
+        """
+        return TimsFrame.from_py_tims_frame(self.__frame_ptr.to_resolution(resolution))
+
+    def vectorized(self, resolution: int = 2) -> 'TimsFrameVectorized':
+        """Convert the frame to a vectorized frame.
+
+        Args:
+            resolution (int, optional): Resolution. Defaults to 2.
+
+        Returns:
+            TimsFrameVectorized: Vectorized frame.
+        """
+        return TimsFrameVectorized.from_py_tims_frame_vectorized(self.__frame_ptr.vectorized(resolution))
+
+    def to_tims_spectra(self) -> List['TimsSpectrum']:
+        """Convert the frame to a list of TimsSpectrum.
+
+        Returns:
+            List[TimsSpectrum]: List of TimsSpectrum.
+        """
+        return [TimsSpectrum.from_py_tims_spectrum(spec) for spec in self.__frame_ptr.to_tims_spectra()]
+
+    def to_windows(self, window_length: float = 10, overlapping: bool = True, min_num_peaks: int = 5,
+                   min_intensity: float = 1) -> List[MzSpectrum]:
+        """Convert the frame to a list of windows.
+
+        Args:
+            window_length (float, optional): Window length. Defaults to 10.
+            overlapping (bool, optional): Whether the windows should overlap. Defaults to True.
+            min_num_peaks (int, optional): Minimum number of peaks in a window. Defaults to 5.
+            min_intensity (float, optional): Minimum intensity of a peak in a window. Defaults to 1.
+
+        Returns:
+            List[MzSpectrum]: List of windows.
+        """
+        return [MzSpectrum.from_py_mz_spectrum(spec) for spec in self.__frame_ptr.to_windows(
+            window_length, overlapping, min_num_peaks, min_intensity)]
+
+    def __repr__(self):
+        return (f"TimsFrame(frame_id={self.__frame_ptr.frame_id}, ms_type={self.__frame_ptr.ms_type}, "
+                f"num_peaks={len(self.__frame_ptr.mz)})")
+
+
+class TimsFrameVectorized:
+    def __init__(self, frame_id: int, ms_type: int, retention_time: float, scan: NDArray[np.int32],
+                 mobility: NDArray[np.float64], tof: NDArray[np.int32],
+                 indices: NDArray[np.int32], intensity: NDArray[np.float64]):
+        """TimsFrameVectorized class.
+
+        Args:
+            frame_id (int): Frame ID.
+            ms_type (int): MS type.
+            retention_time (float): Retention time.
+            scan (NDArray[np.int32]): Scan.
+            mobility (NDArray[np.float64]): Inverse mobility.
+            tof (NDArray[np.int32]): Time of flight.
+            indices (NDArray[np.int32]): Indices.
+            intensity (NDArray[np.float64]): Intensity.
+
+        Raises:
+            AssertionError: If the length of the scan, mobility, tof, indices and intensity arrays are not equal.
+        """
+
+        assert len(scan) == len(mobility) == len(tof) == len(indices) == len(intensity), \
+            "The length of the scan, mobility, tof, indices and intensity arrays must be equal."
+
+        self.__frame_ptr = pims.PyTimsFrameVectorized(frame_id, ms_type, retention_time, scan, mobility, tof, indices,
+                                                      intensity)
+
+    @classmethod
+    def from_py_tims_frame_vectorized(cls, frame: pims.PyTimsFrameVectorized):
+        """Create a TimsFrameVectorized from a PyTimsFrameVectorized.
+
+        Args:
+            frame (pims.PyTimsFrameVectorized): PyTimsFrameVectorized to create the TimsFrameVectorized from.
+
+        Returns:
+            TimsFrameVectorized: TimsFrameVectorized created from the PyTimsFrameVectorized.
+        """
+        instance = cls.__new__(cls)
+        instance.__frame_ptr = frame
+        return instance
+
+    @property
+    def frame_id(self) -> int:
+        """Frame ID.
+
+        Returns:
+            int: Frame ID.
+        """
+        return self.__frame_ptr.frame_id
+
+    @property
+    def ms_type(self) -> str:
+        """MS type.
+
+        Returns:
+            int: MS type.
+        """
+        return self.__frame_ptr.ms_type_as_string
+
+    @property
+    def retention_time(self) -> float:
+        """Retention time.
+
+        Returns:
+            float: Retention time.
+        """
+        return self.__frame_ptr.retention_time
+
+    @property
+    def scan(self) -> NDArray[np.int32]:
+        """Scan.
+
+        Returns:
+            NDArray[np.int32]: Scan.
+        """
+        return self.__frame_ptr.scan
+
+    @property
+    def mobility(self) -> NDArray[np.float64]:
+        """Inverse mobility.
+
+        Returns:
+            NDArray[np.float64]: Inverse mobility.
+        """
+        return self.__frame_ptr.mobility
+
+    @property
+    def tof(self) -> NDArray[np.int32]:
+        """Time of flight.
+
+        Returns:
+            NDArray[np.int32]: Time of flight.
+        """
+        return self.__frame_ptr.tof
+
+    @property
+    def indices(self) -> NDArray[np.int32]:
+        """Indices.
+
+        Returns:
+            NDArray[np.int32]: Indices.
+        """
+        return self.__frame_ptr.indices
+
+    @property
+    def intensity(self) -> NDArray[np.float64]:
+        """Intensity.
+
+        Returns:
+            NDArray[np.float64]: Intensity.
+        """
+        return self.__frame_ptr.values
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """ Data as a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: Data.
+        """
+
+        return pd.DataFrame({
+            'frame': np.repeat(self.frame_id, len(self.scan)),
+            'retention_time': np.repeat(self.retention_time, len(self.scan)),
+            'scan': self.scan,
+            'mobility': self.mobility,
+            'tof': self.tof,
+            'indices': self.indices,
+            'intensity': self.intensity})
+
+    def __repr__(self):
+        return (f"TimsFrameVectorized(frame_id={self.__frame_ptr.frame_id}, ms_type={self.__frame_ptr.ms_type}, "
+                f"num_peaks={len(self.__frame_ptr.indices)})")
+
+    def get_tensor_repr(self, dense=True, zero_indexed=True, re_index=True, scan_max=None, index_max=None):
+        s = self.scan
+        f = self.indices
+        i = self.intensity
+
+        if zero_indexed:
+            s = s - np.min(s)
+            f = f - np.min(f)
+
+        if re_index:
+            f = re_index_indices(f)
+
+        if scan_max is None:
+            m_s = np.max(s) + 1
+        else:
+            m_s = scan_max + 1
+
+        if index_max is None:
+            m_f = np.max(f) + 1
+        else:
+            m_f = index_max + 1
+
+        sv = sp.reorder(sp.SparseTensor(indices=np.c_[s, f], values=i, dense_shape=(m_s, m_f)))
+
+        if dense:
+            return sp.to_dense(sv)
+        else:
+            return sv
---
+++ imspy/imspy/slice.py
@@ -0,0 +1,388 @@
+import numpy as np
+import pandas as pd
+from typing import List
+
+from numpy.typing import NDArray
+from tensorflow import sparse as sp
+
+from pyims.utilities import re_index_indices
+
+import pyims_connector as pims
+from pyims.frame import TimsFrame, TimsFrameVectorized
+from pyims.spectrum import MzSpectrum
+
+
+class TimsSlice:
+    def __init__(self,
+                 frame_id: NDArray[np.int32],
+                 scan: NDArray[np.int32],
+                 tof: NDArray[np.int32],
+                 retention_time: NDArray[np.float64],
+                 mobility: NDArray[np.float64],
+                 mz: NDArray[np.float64],
+                 intensity: NDArray[np.float64]):
+        """Create a TimsSlice.
+
+        Args:
+            frame_id (NDArray[np.int32]): Frame ID.
+            scan (NDArray[np.int32]): Scan.
+            tof (NDArray[np.int32]): TOF.
+            retention_time (NDArray[np.float64]): Retention time.
+            mobility (NDArray[np.float64]): Mobility.
+            mz (NDArray[np.float64]): m/z.
+            intensity (NDArray[np.float64]): Intensity.
+        """
+
+        assert len(frame_id) == len(scan) == len(tof) == len(retention_time) == len(mobility) == len(mz) == len(
+            intensity), "All arrays must have the same length."
+
+        self.__slice_ptr = pims.PyTimsSlice(
+            frame_id, scan, tof, retention_time, mobility, mz, intensity
+        )
+        self.__current_index = 0
+
+    @classmethod
+    def from_py_tims_slice(cls, tims_slice: pims.PyTimsSlice):
+        """Create a TimsSlice from a PyTimsSlice.
+
+        Args:
+            tims_slice (pims.PyTimsSlice): PyTimsSlice to create the TimsSlice from.
+
+        Returns:
+            TimsSlice: TimsSlice created from the PyTimsSlice.
+        """
+        instance = cls.__new__(cls)
+        instance.__slice_ptr = tims_slice
+        instance.__current_index = 0
+        return instance
+
+    @property
+    def first_frame_id(self) -> int:
+        """First frame ID.
+
+        Returns:
+            int: First frame ID.
+        """
+        return self.__slice_ptr.first_frame_id
+
+    @property
+    def last_frame_id(self) -> int:
+        """Last frame ID.
+
+        Returns:
+            int: Last frame ID.
+        """
+        return self.__slice_ptr.last_frame_id
+
+    def __repr__(self):
+        return f"TimsSlice({self.first_frame_id}, {self.last_frame_id})"
+
+    @property
+    def precursors(self):
+        return TimsSlice.from_py_tims_slice(self.__slice_ptr.get_precursors())
+
+    @property
+    def fragments(self):
+        return TimsSlice.from_py_tims_slice(self.__slice_ptr.get_fragments_dda())
+
+    def filter(self, mz_min: float = 0.0, mz_max: float = 2000.0, scan_min: int = 0, scan_max: int = 1000,
+               mobility_min: float = 0.0,
+               mobility_max: float = 2.0,
+               intensity_min: float = 0.0, intensity_max: float = 1e9, num_threads: int = 4) -> 'TimsSlice':
+        """Filter the slice by m/z, scan and intensity.
+
+        Args:
+            mz_min (float): Minimum m/z value.
+            mz_max (float): Maximum m/z value.
+            scan_min (int, optional): Minimum scan value. Defaults to 0.
+            scan_max (int, optional): Maximum scan value. Defaults to 1000.
+            mobility_min (float, optional): Minimum inverse mobility value. Defaults to 0.0.
+            mobility_max (float, optional): Maximum inverse mobility value. Defaults to 2.0.
+            intensity_min (float, optional): Minimum intensity value. Defaults to 0.0.
+            intensity_max (float, optional): Maximum intensity value. Defaults to 1e9.
+            num_threads (int, optional): Number of threads to use. Defaults to 4.
+
+        Returns:
+            TimsSlice: Filtered slice.
+        """
+        return TimsSlice.from_py_tims_slice(
+            self.__slice_ptr.filter_ranged(mz_min, mz_max, scan_min, scan_max, mobility_min, mobility_max,
+                                           intensity_min, intensity_max, num_threads))
+
+    @property
+    def frames(self) -> List[TimsFrame]:
+        """Get the frames.
+
+        Returns:
+            List[TimsFrame]: Frames.
+        """
+        return [TimsFrame.from_py_tims_frame(frame) for frame in self.__slice_ptr.get_frames()]
+
+    def to_resolution(self, resolution: int, num_threads: int = 4) -> 'TimsSlice':
+        """Convert the slice to a given resolution.
+
+        Args:
+            resolution (int): Resolution.
+            num_threads (int, optional): Number of threads to use. Defaults to 4.
+
+        Returns:
+            TimsSlice: Slice with given resolution.
+        """
+        return TimsSlice.from_py_tims_slice(self.__slice_ptr.to_resolution(resolution, num_threads))
+
+    def to_windows(self, window_length: float = 10, overlapping: bool = True, min_num_peaks: int = 5,
+                   min_intensity: float = 1, num_threads: int = 4) -> List[MzSpectrum]:
+        """Convert the slice to a list of windows.
+
+        Args:
+            window_length (float, optional): Window length. Defaults to 10.
+            overlapping (bool, optional): Whether the windows should overlap. Defaults to True.
+            min_num_peaks (int, optional): Minimum number of peaks in a window. Defaults to 5.
+            min_intensity (float, optional): Minimum intensity of a peak in a window. Defaults to 1.
+            num_threads (int, optional): Number of threads to use. Defaults to 1.
+
+        Returns:
+            List[MzSpectrum]: List of windows.
+        """
+        return [MzSpectrum.from_py_mz_spectrum(spec) for spec in self.__slice_ptr.to_windows(
+            window_length, overlapping, min_num_peaks, min_intensity, num_threads)]
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """Get the data as a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: Data.
+        """
+        columns = ['frame', 'scan', 'tof', 'retention_time', 'mobility', 'mz', 'intensity']
+        return pd.DataFrame({c: v for c, v in zip(columns, self.__slice_ptr.to_arrays())})
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.__current_index < self.__slice_ptr.frame_count:
+            frame_ptr = self.__slice_ptr.get_frame_at_index(self.__current_index)
+            self.__current_index += 1
+            if frame_ptr is not None:
+                return TimsFrame.from_py_tims_frame(frame_ptr)
+            else:
+                raise ValueError("Frame pointer is None for valid index.")
+        else:
+            self.__current_index = 0  # Reset for next iteration
+            raise StopIteration
+
+    def vectorized(self, resolution: int = 2, num_threads: int = 4) -> 'TimsSliceVectorized':
+        """Get a vectorized version of the slice.
+
+        Args:
+            resolution (int, optional): Resolution. Defaults to 2.
+            num_threads (int, optional): Number of threads to use. Defaults to 4.
+
+        Returns:
+            TimsSliceVectorized: Vectorized version of the slice.
+        """
+        return TimsSliceVectorized.from_vectorized_py_tims_slice(self.__slice_ptr.vectorized(resolution, num_threads))
+
+    def get_tims_planes(self, tof_max_value: int = 400_000, num_chunks: int = 7, num_threads: int = 4) -> List[
+        'TimsPlane']:
+        return [TimsPlane.from_py_tims_plane(plane) for plane in
+                self.__slice_ptr.to_tims_planes(tof_max_value, num_chunks, num_threads)]
+
+
+class TimsSliceVectorized:
+    def __init__(self):
+        self.__slice_ptr = None
+        self.__current_index = 0
+
+    @classmethod
+    def from_vectorized_py_tims_slice(cls, tims_slice: pims.PyTimsSliceVectorized):
+        """Create a TimsSlice from a PyTimsSlice.
+
+        Args:
+            tims_slice (pims.PyTimsSlice): PyTimsSlice to create the TimsSlice from.
+
+        Returns:
+            TimsSlice: TimsSlice created from the PyTimsSlice.
+        """
+        instance = cls.__new__(cls)
+        instance.__slice_ptr = tims_slice
+        instance.__current_index = 0
+        return instance
+
+    @property
+    def first_frame_id(self) -> int:
+        """First frame ID.
+
+        Returns:
+            int: First frame ID.
+        """
+        return self.__slice_ptr.first_frame_id
+
+    @property
+    def last_frame_id(self) -> int:
+        """Last frame ID.
+
+        Returns:
+            int: Last frame ID.
+        """
+        return self.__slice_ptr.last_frame_id
+
+    @property
+    def precursors(self):
+        return TimsSlice.from_py_tims_slice(self.__slice_ptr.get_precursors())
+
+    @property
+    def fragments(self):
+        return TimsSlice.from_py_tims_slice(self.__slice_ptr.get_fragments_dda())
+
+    @property
+    def frames(self) -> List[TimsFrameVectorized]:
+        """Get the frames.
+
+        Returns:
+            List[TimsFrame]: Frames.
+        """
+        return [TimsFrameVectorized.from_py_tims_frame_vectorized(frame) for frame in
+                self.__slice_ptr.get_vectorized_frames()]
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """Get the data as a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: Data.
+        """
+        columns = ['frame', 'scan', 'tof', 'retention_time', 'mobility', 'index', 'intensity']
+        return pd.DataFrame({c: v for c, v in zip(columns, self.__slice_ptr.to_arrays())})
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.__current_index < self.__slice_ptr.frame_count:
+            frame_ptr = self.__slice_ptr.get_frame_at_index(self.__current_index)
+            self.__current_index += 1
+            if frame_ptr is not None:
+                return TimsFrameVectorized.from_py_tims_frame_vectorized(frame_ptr)
+            else:
+                raise ValueError("Frame pointer is None for valid index.")
+        else:
+            self.__current_index = 0
+            raise StopIteration
+
+    def __repr__(self):
+        return f"TimsSliceVectorized({self.first_frame_id}, {self.last_frame_id})"
+
+    def get_tensor_repr(self, dense=True, zero_index=True, re_index=True, frame_max=None, scan_max=None,
+                        index_max=None):
+
+        frames, scans, _, _, _, indices, intensities = self.__slice_ptr.to_arrays()
+
+        if zero_index:
+            scans = scans - np.min(scans)
+            frames = frames - np.min(frames)
+            indices = indices - np.min(indices)
+
+        if re_index:
+            frames = re_index_indices(frames)
+
+        if scan_max is None:
+            m_s = np.max(scans) + 1
+        else:
+            m_s = scan_max + 1
+
+        if index_max is None:
+            m_i = np.max(indices) + 1
+        else:
+            m_i = index_max + 1
+
+        if frame_max is None:
+            m_f = np.max(frames) + 1
+        else:
+            m_f = frame_max + 1
+
+        sv = sp.reorder(
+            sp.SparseTensor(indices=np.c_[frames, scans, indices], values=intensities, dense_shape=(m_f, m_s, m_i)))
+
+        if dense:
+            return sp.to_dense(sv)
+        else:
+            return sv
+
+
+class TimsPlane:
+    def __init__(self):
+        self.__plane_ptr = None
+
+    @classmethod
+    def from_py_tims_plane(cls, plane: pims.PyTimsPlane):
+        """Create a TimsPlane from a PyTimsPlane.
+
+        Args:
+            plane (pims.PyTimsPlane): PyTimsPlane to create the TimsPlane from.
+
+        Returns:
+            TimsPlane: TimsPlane created from the PyTimsPlane.
+        """
+        instance = cls.__new__(cls)
+        instance.__plane_ptr = plane
+        return instance
+
+    @property
+    def mz_mean(self):
+        return self.__plane_ptr.mz_mean
+
+    @property
+    def mz_std(self):
+        return self.__plane_ptr.mz_std
+
+    @property
+    def tof_mean(self):
+        return self.__plane_ptr.tof_mean
+
+    @property
+    def tof_std(self):
+        return self.__plane_ptr.tof_std
+
+    @property
+    def frame_ids(self):
+        return self.__plane_ptr.frame_ids
+
+    @property
+    def scans(self):
+        return self.__plane_ptr.scans
+
+    @property
+    def intensities(self):
+        return self.__plane_ptr.intensity
+
+    @property
+    def retention_times(self):
+        return self.__plane_ptr.retention_times
+
+    @property
+    def mobilities(self):
+        return self.__plane_ptr.mobilities
+
+    @property
+    def num_points(self):
+        return len(self.frame_ids)
+
+    @property
+    def df(self):
+        return pd.DataFrame({
+            'frame': self.frame_ids,
+            'scan': self.scans,
+            'retention_time': self.retention_times,
+            'mobility': self.mobilities,
+            'intensity': self.intensities
+        })
+
+    def __repr__(self):
+        return (f"TimsPlane(mz_mean: "
+                f"{np.round(self.mz_mean, 4)}, "
+                f"mz_std: {np.round(self.mz_std, 4)},"
+                f" tof_mean: {np.round(self.tof_mean, 4)}, "
+                f"tof_std: {np.round(self.tof_std, 4)}, "
+                f"num_points: {len(self.frame_ids)})")
