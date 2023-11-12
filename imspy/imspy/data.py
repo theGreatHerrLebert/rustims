b0507688b223c26d7bbd327bcecd2dfaef2e9779
theGreatHerrLebert <davidteschner@googlemail.com>
2023-11-12 15:42:33 +0100

    fixing export

--- imspy/imspy/data.py
+++ imspy/imspy/data.py
@@ -1,4 +1,5 @@
 from .frame import TimsFrame
 from .spectrum import TimsSpectrum, MzSpectrum
-from .handle import TimsDataset, TimsDatasetDDA, TimsDatasetDIA
+from .handle import TimsDataset, TimsDatasetDIA
 from .slice import TimsSlice, TimsSliceVectorized
+from .dda import TimsDatasetDDA, FragmentDDA
