7974fd4c619baf12c9ba6a38e41cada75c6ecfe9
theGreatHerrLebert <davidteschner@googlemail.com>
2023-12-11 17:35:11 +0100

    adding dia aquisition

--- imspy/imspy/simulation/aquisition.py
+++ imspy/imspy/simulation/aquisition.py
@@ -103,6 +103,39 @@ class TimsTofAcquisitionBuilderDDA(TimsTofAcquisitionBuilder, ABC):
                                                                  precursor_every=self.precursor_every, verbose=verbose)
 
 
+class TimsTofAcquisitionBuilderDIA(TimsTofAcquisitionBuilder, ABC):
+    def __init__(self,
+                 verbose: bool = True,
+                 precursor_every: int = 17,
+                 gradient_length=50 * 60,
+                 rt_cycle_length=0.1054,
+                 im_lower=0.6,
+                 im_upper=1.5,
+                 num_scans=927,
+                 mz_lower: float = 100,
+                 mz_upper: float = 1700):
+        super().__init__(gradient_length, rt_cycle_length, im_lower, im_upper, num_scans)
+        self.scan_table = None
+        self.frame_table = None
+        self.acquisition_mode = AcquisitionMode('DIA')
+        self.verbose = verbose
+        self.mz_lower = mz_lower
+        self.mz_upper = mz_upper
+        self.precursor_every = precursor_every
+
+        self._setup(verbose=verbose)
+
+    def calculate_frame_types(self, table: pd.DataFrame, verbose: bool = True) -> NDArray:
+        if verbose:
+            print(f'calculating frame types, precursor frame will be taken every {self.precursor_every} rt cycles.')
+        return np.array([0 if (x - 1) % (self.precursor_every + 1) == 0 else 9 for x in table.frame_id])
+
+    def _setup(self, verbose: bool = True):
+        self.frame_table = self.generate_frame_table(verbose=verbose)
+        self.scan_table = self.generate_scan_table(verbose=verbose)
+        self.frame_table['ms_type'] = self.calculate_frame_types(table=self.frame_table, verbose=verbose)
+
+
 class TimsTofAcquisitionBuilderMIDIA(TimsTofAcquisitionBuilder, ABC):
     def __init__(self,
                  verbose: bool = True,
