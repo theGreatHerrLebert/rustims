10f57cb5a719e288c926b8a3d8fdbf9fb77c4abe
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-05 16:38:03 +0100

    adding quad impl

--- imspy_connector/src/py_quadrupole.rs
+++ imspy_connector/src/py_quadrupole.rs
@@ -14,7 +14,6 @@ impl PyTimsTransmissionDIA {
     #[new]
     pub fn new(frame: Vec<i32>,
                frame_window_group: Vec<i32>,
-
                window_group: Vec<i32>,
                scan_start: Vec<i32>,
                scan_end: Vec<i32>,
@@ -45,4 +44,25 @@ impl PyTimsTransmissionDIA {
     pub fn transmit_tims_frame(&self, frame: PyTimsFrame, min_probability: Option<f64>) -> PyTimsFrame {
         PyTimsFrame { inner: self.inner.transmit_tims_frame(&frame.inner, min_probability) }
     }
+
+    pub fn get_setting(&self, window_group: i32, scan_id: i32) -> (f64, f64) {
+        let result = self.inner.get_setting(window_group, scan_id);
+        match result {
+            Some((a, b)) => (*a, *b),
+            None => (-1.0, -1.0)
+        }
+    }
+
+    pub fn frame_to_window_group(&self, frame_id: i32) -> i32 {
+        self.inner.frame_to_window_group(frame_id)
+    }
+
+    pub fn is_transmitted(&self, frame_id: i32, scan_id: i32, mz: f64) -> bool {
+        self.inner.is_transmitted(frame_id, scan_id, mz)
+    }
+}
+
+#[pyfunction]
+pub fn apply_transmission(midpoint: f64, window_length: f64, mz: Vec<f64>, k: Option<f64>) -> Vec<f64> {
+    mscore::apply_transmission(midpoint, window_length, k, mz)
 }
