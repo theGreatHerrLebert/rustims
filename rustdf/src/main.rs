48eed5991541a0a646c0b0766b97701e71e169d1
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-06 15:04:34 +0100

    adding main for testing

--- rustdf/src/main.rs
+++ rustdf/src/main.rs
@@ -1,18 +1,40 @@
-use std::env;
 use rustdf::data::simulation::{TimsTofSyntheticsDIA};
 use std::path::Path;
+use clap::Parser;
+
+/// Create synthetic DIA proteomics experiment data
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to synthetic_data.db
+    #[arg(short, long)]
+    path: String,
+
+    /// Number of threads to use
+    #[arg(short, long, default_value_t = 64)]
+    num_threads: usize,
+
+    /// Fragment the precursors into product ions
+    #[arg(short, long, default_value_t = false)]
+    fragment: bool,
+}
+
 
 fn main() {
-    let _args: Vec<String> = env::args().collect();
 
-    let db_path_str = "/media/hd02/data/SIM/TEST-SIM-MONSTER-2/synthetic_data.db";
-    let path = Path::new(db_path_str);
+    let args = Args::parse();
+
+    let db_path_str = args.path;
+    let path = Path::new(&db_path_str);
+    let num_threads = args.num_threads;
+    let fragment = args.fragment;
+
     let experiment = TimsTofSyntheticsDIA::new(path).unwrap();
     let first_frames = experiment.synthetics.frames.iter().map(|x| x.frame_id.clone()).take(4096).collect::<Vec<_>>();
 
     // go over the frames in batches of 256
     for frame_batch in first_frames.chunks(32) {
-        let frames = experiment.build_frames(frame_batch.to_vec(), false, 64);
+        let frames = experiment.build_frames(frame_batch.to_vec(), fragment, num_threads);
         for frame in frames {
             println!("frame_id: {}", frame.frame_id);
             println!("frame: {}", frame);
