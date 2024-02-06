4891f297a42d6e96f340aae711c8162d159d591b
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-06 17:34:04 +0100

    adding TimsTofSyntheticsDIA

--- imspy_connector/src/lib.rs
+++ imspy_connector/src/lib.rs
@@ -16,7 +16,7 @@ use crate::py_mz_spectrum::{PyMzSpectrum, PyIndexedMzSpectrum, PyTimsSpectrum, P
 use crate::py_tims_frame::{PyTimsFrame, PyTimsFrameVectorized, PyRawTimsFrame};
 use crate::py_tims_slice::{PyTimsPlane, PyTimsSlice, PyTimsSliceVectorized};
 use crate::py_dda::{PyTimsDatasetDDA, PyTimsFragmentDDA};
-use crate::py_simulation::PyTimsTofSynthetics;
+use crate::py_simulation::{PyTimsTofSynthetics, PyTimsTofSyntheticsDIA};
 pub use py_chemistry::{generate_precursor_spectrum, generate_precursor_spectra, calculate_monoisotopic_mass, calculate_b_y_ion_series, simulate_charge_state_for_sequence, simulate_charge_states_for_sequences};
 use crate::py_quadrupole::{PyTimsTransmissionDIA, apply_transmission};
 
@@ -39,6 +39,7 @@ fn imspy_connector(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyAcquisitionMode>()?;
     m.add_class::<PyTimsTofSynthetics>()?;
     m.add_class::<PyTimsTransmissionDIA>()?;
+    m.add_class::<PyTimsTofSyntheticsDIA>()?;
     m.add_function(wrap_pyfunction!(generate_precursor_spectrum, m)?)?;
     m.add_function(wrap_pyfunction!(generate_precursor_spectra, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_monoisotopic_mass, m)?)?;
