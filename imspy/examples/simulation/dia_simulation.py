4a4483e367c2271e95526d637cd01e0030cef79c
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-13 16:47:53 +0100

    fixing mem usage of GPU

--- imspy/examples/simulation/dia_simulation.py
+++ imspy/examples/simulation/dia_simulation.py
@@ -19,6 +19,20 @@ from imspy.algorithm.ionization.predictors import BinomialChargeStateDistributio
 
 from pathlib import Path
 
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(f"{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 os.environ["WANDB_SILENT"] = "true"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
