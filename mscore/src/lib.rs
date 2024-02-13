3ab89fb659e35fe5bdd75546d39a06c7c8f3834a
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-13 15:23:51 +0100

    refactoring

--- mscore/src/lib.rs
+++ mscore/src/lib.rs
@@ -1,23 +1,3 @@
-// chemistry module
-pub mod chemistry {
-    pub mod atoms;
-    pub mod amino_acids;
-    pub mod unimod;
-    pub mod aa_sequence;
-    pub mod constants;
-    pub mod formulas;
-}
-
-// algorithm module
-pub mod algorithm {
-    pub mod quadrupole;
-    pub mod isotope_distributions;
-    pub mod aa_sequence;
-}
-
-// data module
-pub mod data {
-    pub mod mz_spectrum;
-    pub mod tims_frame;
-    pub mod tims_slice;
-}
+pub mod data;
+pub mod algorithm;
+pub mod chemistry;
