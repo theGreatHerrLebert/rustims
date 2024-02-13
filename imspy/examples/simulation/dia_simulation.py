b17a8819faf2660b7c86c6638fd383d27e6b8762
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-13 16:59:37 +0100

    setting hard GPU mem limit

--- imspy/examples/simulation/dia_simulation.py
+++ imspy/examples/simulation/dia_simulation.py
@@ -24,14 +24,16 @@ import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
-        # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(f"{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
+        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * 4)]
+        )
+        print("GPU memory restricted to 4GB.")
     except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
+        # Virtual devices must be set before GPUs have been initialized
         print(e)
+
 
 os.environ["WANDB_SILENT"] = "true"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
