892655c66a9eafdbdc002e9ebebe2b1af997f691
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-16 15:43:06 +0100

    adding info table to dia return

--- imspy/examples/simulation/dia_simulation.py
+++ imspy/examples/simulation/dia_simulation.py
@@ -9,7 +9,7 @@ from imspy.simulation.aquisition import TimsTofAcquisitionBuilderDIA
 from imspy.algorithm import DeepPeptideIonMobilityApex, DeepChromatographyApex
 from imspy.algorithm import (load_tokenizer_from_resources, load_deep_retention_time, load_deep_ccs_predictor)
 
-from imspy.simulation.utility import generate_events, python_list_to_json_string, sequence_to_all_ions
+from imspy.simulation.utility import generate_events, python_list_to_json_string
 from imspy.simulation.isotopes import generate_isotope_patterns_rust
 from imspy.simulation.utility import (get_z_score_for_percentile, get_frames_numba, get_scans_numba,
                                       accumulated_intensity_cdf_numba)
--- imspy/imspy/timstof/dia.py
+++ imspy/imspy/timstof/dia.py
@@ -11,11 +11,21 @@ class TimsDatasetDIA(TimsDataset):
         self.__dataset = pims.PyTimsDatasetDIA(self.data_path, self.binary_path)
 
     @property
-    def pasef_meta_data(self):
+    def dia_ms_ms_windows(self):
         """Get PASEF meta data for DIA.
 
         Returns:
             pd.DataFrame: PASEF meta data.
         """
         return pd.read_sql_query("SELECT * from DiaFrameMsMsWindows",
+                                 sqlite3.connect(self.data_path + "/analysis.tdf"))
+
+    @property
+    def dia_ms_ms_info(self):
+        """Get DIA MS/MS info.
+
+        Returns:
+            pd.DataFrame: DIA MS/MS info.
+        """
+        return pd.read_sql_query("SELECT * from DiaFrameMsMsInfo",
                                  sqlite3.connect(self.data_path + "/analysis.tdf"))
