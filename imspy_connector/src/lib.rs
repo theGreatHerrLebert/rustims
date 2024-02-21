f836cd402ecdad8c19ebd50acf551411ec797ea2
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-21 11:04:40 +0100

    fixing

--- imspy_connector/src/lib.rs
+++ imspy_connector/src/lib.rs
@@ -54,5 +54,6 @@ fn imspy_connector(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_chemistry::find_unimod_annotations, m)?)?;
     m.add_function(wrap_pyfunction!(py_chemistry::find_unimod_annotations_par, m)?)?;
     m.add_function(wrap_pyfunction!(py_chemistry::sequence_to_all_ions_ims, m)?)?;
+    m.add_function(wrap_pyfunction!(py_chemistry::reshape_prosit_array, m)?)?;
     Ok(())
 }
