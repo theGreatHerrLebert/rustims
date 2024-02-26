90b1a387ef34204572728f1ba0870fd84fd1ce23
theGreatHerrLebert <davidteschner@googlemail.com>
2024-02-26 18:29:20 +0100

    adding AA sequence

--- imspy_connector/src/lib.rs
+++ imspy_connector/src/lib.rs
@@ -18,6 +18,7 @@ use crate::py_tims_slice::{PyTimsPlane, PyTimsSlice, PyTimsSliceVectorized};
 use crate::py_dda::{PyTimsDatasetDDA, PyTimsFragmentDDA};
 use crate::py_simulation::{PyTimsTofSyntheticsPrecursorFrameBuilder, PyTimsTofSyntheticsFrameBuilderDIA, PyTimsTofSyntheticsDataHandle};
 pub use py_chemistry::{generate_precursor_spectrum, generate_precursor_spectra, calculate_monoisotopic_mass, calculate_b_y_ion_series, simulate_charge_state_for_sequence, simulate_charge_states_for_sequences};
+use crate::py_chemistry::PyAminoAcidSequence;
 use crate::py_dia::PyTimsDatasetDIA;
 use crate::py_quadrupole::{PyTimsTransmissionDIA, apply_transmission, PyTimsTofCollisionEnergyDIA};
 
@@ -44,6 +45,7 @@ fn imspy_connector(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyTimsTofSyntheticsFrameBuilderDIA>()?;
     m.add_class::<PyTimsTofCollisionEnergyDIA>()?;
     m.add_class::<PyTimsTofSyntheticsDataHandle>()?;
+    m.add_class::<PyAminoAcidSequence>()?;
     m.add_function(wrap_pyfunction!(generate_precursor_spectrum, m)?)?;
     m.add_function(wrap_pyfunction!(generate_precursor_spectra, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_monoisotopic_mass, m)?)?;
