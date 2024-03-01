cfc811e53160f55327883840074f1c7d00301bf1
theGreatHerrLebert <davidteschner@googlemail.com>
2024-03-01 11:28:48 +0100

    adding unimod

--- imspy/imspy/core/chemistry/amino_acids.py
+++ imspy/imspy/core/chemistry/amino_acids.py
@@ -3,3 +3,5 @@ ims = imspy_connector.py_amino_acids
 
 
 AMINO_ACID_MASSES = ims.get_amino_acid_mono_isotopic_masses()
+AMINO_ACIDS = ims.get_amino_acids()
+AMINO_ACID_ATOMIC_COMPOSITIONS = ims.get_amino_acid_atomic_compositions()
