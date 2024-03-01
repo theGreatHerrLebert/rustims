975f45de2d9dbe06dfccaf578bca2a96ce342d6e
theGreatHerrLebert <davidteschner@googlemail.com>
2024-03-01 09:39:22 +0100

    fixing missing return

--- imspy/imspy/core/chemistry/mobility.py
+++ imspy/imspy/core/chemistry/mobility.py
@@ -12,7 +12,7 @@ def one_over_k0_to_ccs(one_over_k0, mz, charge, mass_gas=28.013, temp=31.85, t_d
     :param temp: temperature of the drift gas in C°
     :param t_diff: factor to translate from C° to K
     """
-    ims.one_over_reduced_mobility_to_ccs(one_over_k0, mz, charge, mass_gas, temp, t_diff)
+    return ims.one_over_reduced_mobility_to_ccs(one_over_k0, mz, charge, mass_gas, temp, t_diff)
 
 
 def ccs_to_k0(ccs, mz, charge, mass_gas=28.013, temp=31.85, t_diff=273.15):
@@ -25,4 +25,4 @@ def ccs_to_k0(ccs, mz, charge, mass_gas=28.013, temp=31.85, t_diff=273.15):
     :param temp: temperature of the drift gas in C°
     :param t_diff: factor to translate from C° to K
     """
-    ims.ccs_to_reduced_mobility(ccs, mz, charge, mass_gas, temp, t_diff)
+    return ims.ccs_to_reduced_mobility(ccs, mz, charge, mass_gas, temp, t_diff)
