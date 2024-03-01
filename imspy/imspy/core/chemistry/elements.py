28d53094c2da9e5968df1a5406c67c47f7e34de0
theGreatHerrLebert <davidteschner@googlemail.com>
2024-03-01 11:37:37 +0100

    fixing

--- imspy/imspy/core/chemistry/elements.py
+++ imspy/imspy/core/chemistry/elements.py
@@ -3,8 +3,8 @@ import imspy_connector
 ims = imspy_connector.py_elements
 
 ELEMENTAL_MONO_ISOTOPIC_MASSES = ims.get_elemental_mono_isotopic_weight_map()
-ELEMENTAL_ISOTOPIC_MASSES = ims.get_elemental_isotopes_weight_map()
-ELEMENTAL_ISOTOPIC_ABUNDANCES = ims.get_elemental_isotopes_abundance_map()
+ELEMENTAL_ISOTOPIC_MASSES = ims.get_elemental_isotope_weight_map()
+ELEMENTAL_ISOTOPIC_ABUNDANCES = ims.get_elemental_isotope_abundance_map()
 
 
 def get_elemental_isotopes_abundance_map() -> Dict[str, List[float]]:
