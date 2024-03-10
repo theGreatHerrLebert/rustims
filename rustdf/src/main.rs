9e01673e93e9505b83a0a1623a17b99da339e866
theGreatHerrLebert <davidteschner@googlemail.com>
2024-03-10 13:54:05 +0100

    testimg

--- rustdf/src/main.rs
+++ rustdf/src/main.rs
@@ -38,7 +38,7 @@ fn main() {
     let fragment = args.fragment;
 
     let experiment = TimsTofSyntheticsFrameBuilderDIA::new(path).unwrap();
-    let first_frames = experiment.precursor_frame_builder.frames.iter().map(|x| x.frame_id.clone()).take(args.num_frames).collect::<Vec<_>>();
+    let first_frames = experiment.precursor_frame_builder.frames.iter().map(|x| x.frame_id.clone()).skip(100).take(args.num_frames).collect::<Vec<_>>();
 
     /*
     for frame in first_frames.iter() {
