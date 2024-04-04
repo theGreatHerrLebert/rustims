b0f2f4b32328838544c7eef31030976fec73d0c4
theGreatHerrLebert <davidteschner@googlemail.com>
2024-04-04 14:04:35 +0200

    fixing

--- imspy/examples/simulation/jobs/add_noise_from_real_data.py
+++ imspy/examples/simulation/jobs/add_noise_from_real_data.py
@@ -29,21 +29,19 @@ def get_precursor_noise(handle, sample_fraction: float = 0.5, max_intensity: flo
     return frame_sampled_a + frame_sampled_b
 
 
-def get_fragment_noise(handle, target_wg, sample_fraction: float = 0.5, max_intensity: float = 30) -> TimsFrame:
+def get_fragment_noise(handle, target_wg, frame_to_window_group, sample_fraction: float = 0.5, max_intensity: float = 30) -> TimsFrame:
     """Get noise from fragment frames.
 
     Args:
         handle: TimsTOF handle.
         target_wg: Target window group.
+        frame_to_window_group: Frame to window group.
         sample_fraction (float): Sample fraction.
         max_intensity (float): Maximum intensity.
 
     Returns:
         TimsFrame: Frame.
     """
-    print(handle)
-    wg = handle.dia_ms_ms_info
-    frame_to_window_group = dict(zip(wg.Frame, wg.WindowGroup))
     filtered_dict = dict(filter(lambda item: item[1] == target_wg, frame_to_window_group.items()))
     frame_ids = [item[0] for item in filtered_dict.items()]
 
@@ -82,11 +80,14 @@ def add_real_data_noise_to_frames(
     window_group_dict = dict(zip(d['frame'], d['window_group']))
     fragment_frames = set(window_group_dict.keys())
 
+    wg = acquisition_builder.tdf_writer.helper_handle.dia_ms_ms_info
+    frame_to_window_group = dict(zip(wg.Frame, wg.WindowGroup))
+
     for frame in frames:
         if frame.frame_id in fragment_frames:
             target_wg = window_group_dict[frame.frame_id]
-            noise = get_fragment_noise(acquisition_builder.tdf_writer.helper_handle, target_wg, sample_fraction,
-                                       intensity_max)
+            noise = get_fragment_noise(acquisition_builder.tdf_writer.helper_handle, target_wg, frame_to_window_group,
+                                       sample_fraction, intensity_max)
             r_list.append(frame + noise)
         else:
             noise = get_precursor_noise(acquisition_builder.tdf_writer.helper_handle, sample_fraction, intensity_max)
