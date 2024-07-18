b45e0f5454b4ee25f284af6502a47572ca34acaa
theGreatHerrLebert <davidteschner@googlemail.com>
2024-07-18 15:32:48 +0200

    fixing

--- imspy/imspy/timstof/dbsearch/imspy_sage_parser.py
+++ imspy/imspy/timstof/dbsearch/imspy_sage_parser.py
@@ -3,14 +3,6 @@ import logging
 import argparse
 import sys
 import os
-import warnings
-
-# import the necessary packages
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-
-from sagepy.qfdr.tdc import target_decoy_competition_pandas
 
 from imspy.algorithm import DeepPeptideIonMobilityApex, load_deep_ccs_predictor, load_tokenizer_from_resources
 from imspy.algorithm.rt.predictors import DeepChromatographyApex, load_deep_retention_time_predictor
