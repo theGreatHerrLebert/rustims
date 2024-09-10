301273e21113ff5f7bc3eb2e8e58f645692b91de
theGreatHerrLebert <davidteschner@googlemail.com>
2024-09-10 11:20:18 +0200

    name space mangling

--- imspy/imspy/algorithm/__init__.py
+++ imspy/imspy/algorithm/__init__.py
@@ -1,7 +1,7 @@
 # from .mixture import GaussianMixtureModel
-from imspy.algorithm.ccs.predictors import DeepPeptideIonMobilityApex, GRUCCSPredictor, load_deep_ccs_predictor
-from imspy.algorithm.ionization.predictors import DeepChargeStateDistribution, GRUChargeStatePredictor, load_deep_charge_state_predictor
+from .ccs.predictors import DeepPeptideIonMobilityApex, GRUCCSPredictor, load_deep_ccs_predictor
+from .ionization.predictors import DeepChargeStateDistribution, GRUChargeStatePredictor, load_deep_charge_state_predictor
 
-from imspy.algorithm.ccs.predictors import predict_inverse_ion_mobility
-from imspy.algorithm.rt.predictors import predict_retention_time
-from imspy.algorithm.intensity.predictors import predict_intensities_prosit
+from .ccs.predictors import predict_inverse_ion_mobility
+from .rt.predictors import predict_retention_time
+from .intensity.predictors import predict_intensities_prosit
