8cbad45d5c5ec22d91282754b02c46295ea69181
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2024-10-16 08:41:23 +0000

    deploy: b5560945bca3647767bd5edff183b6013deef38a

--- main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
+++ main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "mscore":[],
-"rustdf":[["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]
+"rustdf":[["impl <a class=\"trait\" href=\"mscore/timstof/collision/trait.TimsTofCollisionEnergy.html\" title=\"trait mscore::timstof::collision::TimsTofCollisionEnergy\">TimsTofCollisionEnergy</a> for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
