ca9fee5b773c2063e0ccb50add91e57d61004f39
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2025-01-06 08:32:08 +0000

    deploy: 208a6eb55f5d30470d64f4cde631fe0cd74ba9ee

---
+++ v0.3.0-alpha/rustdf/sim/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["containers","dia","handle","precursor","utility"]};
---
+++ v0.3.0-alpha/trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mscore",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"mscore/data/peptide/enum.FragmentType.html\" title=\"enum mscore::data::peptide::FragmentType\">FragmentType</a>"]]],["rustms",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"rustms/proteomics/peptide/enum.FragmentType.html\" title=\"enum rustms::proteomics::peptide::FragmentType\">FragmentType</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[296,309]}
