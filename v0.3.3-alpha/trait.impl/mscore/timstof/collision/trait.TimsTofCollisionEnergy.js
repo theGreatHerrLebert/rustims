723b40126dc96614932ad1f20250778f441fb222
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2025-01-07 09:39:56 +0000

    deploy: 4c0a179de3312222f663a18fd8482b40778067cb

---
+++ v0.3.3-alpha/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mscore",[]],["rustdf",[["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[13,243]}
