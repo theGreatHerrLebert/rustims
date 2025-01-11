a1e1c6a964f8f4555e74ca640d5f014264a016b6
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2025-01-11 10:13:14 +0000

    deploy: 368114b48a463a2534a7114f894fe2115a87fb96

---
+++ v0.3.6-alpha/trait.impl/core/ops/arith/trait.Sub.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mscore",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"mscore/data/spectrum/struct.MzSpectrum.html\" title=\"struct mscore::data::spectrum::MzSpectrum\">MzSpectrum</a>"]]],["rustms",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"rustms/ms/spectrum/struct.MzSpectrum.html\" title=\"struct rustms::ms::spectrum::MzSpectrum\">MzSpectrum</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[302,299]}
