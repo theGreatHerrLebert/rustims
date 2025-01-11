c0442d07f0351774070787e6cb480e4b5ba23c4e
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2025-01-11 10:13:09 +0000

    deploy: 368114b48a463a2534a7114f894fe2115a87fb96

--- main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
+++ main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mscore",[]],["rustdf",[["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]]]);
+    var implementors = Object.fromEntries([["mscore",[]],["rustdf",[["impl <a class=\"trait\" href=\"mscore/timstof/collision/trait.TimsTofCollisionEnergy.html\" title=\"trait mscore::timstof::collision::TimsTofCollisionEnergy\">TimsTofCollisionEnergy</a> for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[13,243]}
+//{"start":57,"fragment_lengths":[13,401]}
