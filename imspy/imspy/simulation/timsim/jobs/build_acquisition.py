43646cb374604298a5f1a2cf9c860e23c239a6b3
theGreatHerrLebert <davidteschner@googlemail.com>
2025-03-11 10:41:29 +0100

    adding dda mode to acq builder job

--- imspy/imspy/simulation/timsim/jobs/build_acquisition.py
+++ imspy/imspy/simulation/timsim/jobs/build_acquisition.py
@@ -1,6 +1,6 @@
-from imspy.simulation.acquisition import TimsTofAcquisitionBuilderDIA
+from imspy.simulation.acquisition import TimsTofAcquisitionBuilder, TimsTofAcquisitionBuilderDDA, TimsTofAcquisitionBuilderDIA
 from imspy.simulation.utility import read_acquisition_config
-from imspy.timstof import TimsDatasetDIA
+from imspy.timstof import TimsDataset, TimsDatasetDIA
 
 
 def build_acquisition(
@@ -15,7 +15,7 @@ def build_acquisition(
         round_collision_energy: bool = True,
         collision_energy_decimals: int = 0,
         use_bruker_sdk: bool = True,
-) -> TimsTofAcquisitionBuilderDIA:
+) -> TimsTofAcquisitionBuilder:
     """Build acquisition object from reference path.
 
     Args:
@@ -36,28 +36,48 @@ def build_acquisition(
     """
 
     acquisition_type = acquisition_type.lower()
+    known_acquisitions = ['dda', 'dia', 'midia', 'slice', 'synchro']
+    assert acquisition_type.lower() in known_acquisitions, f"Unknown acquisition type: {acquisition_type}, " \
+                                                              f"must be one of {known_acquisitions}."
 
-    assert acquisition_type in ['dia', 'midia', 'slice', 'synchro'], \
-        f"Acquisition type must be 'dia', 'midia', 'slice' or 'synchro', was {acquisition_type}"
+    if acquisition_type.lower() == 'dda':
 
-    config = read_acquisition_config(acquisition_name=acquisition_type)['acquisition']
+        # Load reference dataset
+        reference_ds = TimsDataset(reference_path)
 
-    if gradient_length is not None:
-        config['gradient_length'] = gradient_length
+        if gradient_length is None:
+            gradient_length = 3600.0
 
-    if verbose:
-        print(f"Using acquisition type: {acquisition_type}")
-        print(config)
+        rt_cycle_length = 0.109
 
-    ref_ds = TimsDatasetDIA(reference_path, in_memory=reference_in_memory, use_bruker_sdk=use_bruker_sdk)
+        return TimsTofAcquisitionBuilderDDA(
+            path=path,
+            reference_ds=reference_ds,
+            exp_name=exp_name,
+            verbose=verbose,
+            gradient_length=gradient_length,
+            rt_cycle_length=rt_cycle_length
+        )
 
-    return TimsTofAcquisitionBuilderDIA.from_config(
-        path=path,
-        reference_ds=ref_ds,
-        exp_name=exp_name,
-        config=config,
-        verbose=verbose,
-        use_reference_layout=use_reference_ds_layout,
-        round_collision_energy=round_collision_energy,
-        collision_energy_decimals=collision_energy_decimals
-    )
+    else:
+        config = read_acquisition_config(acquisition_name=acquisition_type)['acquisition']
+
+        if gradient_length is not None:
+            config['gradient_length'] = gradient_length
+
+        if verbose:
+            print(f"Using acquisition type: {acquisition_type}")
+            print(config)
+
+        ref_ds = TimsDatasetDIA(reference_path, in_memory=reference_in_memory, use_bruker_sdk=use_bruker_sdk)
+
+        return TimsTofAcquisitionBuilderDIA.from_config(
+            path=path,
+            reference_ds=ref_ds,
+            exp_name=exp_name,
+            config=config,
+            verbose=verbose,
+            use_reference_layout=use_reference_ds_layout,
+            round_collision_energy=round_collision_energy,
+            collision_energy_decimals=collision_energy_decimals
+        )
