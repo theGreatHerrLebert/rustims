c90ec39cdaf30c3b5561cb0ff52ab9e19b065ab1
theGreatHerrLebert <davidteschner@googlemail.com>
2025-03-13 17:00:56 +0100

    adding default cpu all to sim handle

--- imspy/imspy/simulation/handle.py
+++ imspy/imspy/simulation/handle.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 import imspy_connector
@@ -15,7 +16,24 @@ class TimsTofSyntheticsDataHandleRust:
     def __repr__(self):
         return f"TimsTofSyntheticsDataHandleRust(path={self.path})"
 
-    def get_transmitted_ions(self, num_threads: int=4, dda: bool=False) -> pd.DataFrame:
+    def get_transmitted_ions(self, num_threads: int=-1, dda: bool=False) -> pd.DataFrame:
+        """
+        Get transmitted ions, needed to create fragment ion table for simulation.
+        Args:
+            num_threads: Number of threads to use for the calculation.
+            dda: if true, the ions are sampled from a DDA experiment, otherwise from a DIA experiment.
+
+        Returns:
+            pd.DataFrame: DataFrame with the following columns:
+                - peptide_id: Peptide ID.
+                - ion_id: Ion ID.
+                - sequence: Peptide sequence.
+                - charge: Ion charge.
+                - collision_energy: Collision energy.
+        """
+        if num_threads == -1:
+            num_threads = os.cpu_count()
+
         peptide_ids, ion_ids, sequences, charges, collision_energies = self.__handle.get_transmitted_ions(num_threads, dda)
         return pd.DataFrame({
             'peptide_id': peptide_ids,
