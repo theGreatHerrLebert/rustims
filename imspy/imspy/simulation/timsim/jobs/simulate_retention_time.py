4d502f5970a79a5903dba7757f52267d940a4674
zix-xiao <cory.zixuan.xiao@outlook.com>
2025-05-14 15:44:59 +0200

    add koina support to retention time predictors

--- imspy/imspy/simulation/timsim/jobs/simulate_retention_time.py
+++ imspy/imspy/simulation/timsim/jobs/simulate_retention_time.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
-from imspy.algorithm.rt.predictors import DeepChromatographyApex, load_deep_retention_time_predictor
+from imspy.algorithm.rt.predictors import (
+    DeepChromatographyApex,
+    load_deep_retention_time_predictor,
+    predict_retention_time_with_koina,
+)
 from imspy.algorithm.utility import load_tokenizer_from_resources
 
 
 def simulate_retention_times(
-        peptides: pd.DataFrame,
-        verbose: bool = False,
-        gradient_length: float = 60 * 60,
+    peptides: pd.DataFrame,
+    verbose: bool = False,
+    use_koina_model: str = None,
+    gradient_length: float = 60 * 60,
 ) -> pd.DataFrame:
     """Simulate retention times.
 
@@ -22,18 +27,24 @@ def simulate_retention_times(
 
     if verbose:
         print("Simulating retention times...")
+    if use_koina_model is not None:
+        peptide_rt = predict_retention_time_with_koina(
+            model_name=use_koina_model,
+            data=peptides,
+            gradient_length=gradient_length,
+        )
+    else:
+        # create RTColumn instance
+        RTColumn = DeepChromatographyApex(
+            model=load_deep_retention_time_predictor(),
+            tokenizer=load_tokenizer_from_resources(tokenizer_name="tokenizer-ptm"),
+            verbose=verbose,
+        )
 
-    # create RTColumn instance
-    RTColumn = DeepChromatographyApex(
-        model=load_deep_retention_time_predictor(),
-        tokenizer=load_tokenizer_from_resources(tokenizer_name='tokenizer-ptm'),
-        verbose=verbose
-    )
-
-    # predict rts
-    peptide_rt = RTColumn.simulate_separation_times_pandas(
-        data=peptides,
-        gradient_length=gradient_length,
-    )
+        # predict rts
+        peptide_rt = RTColumn.simulate_separation_times_pandas(
+            data=peptides,
+            gradient_length=gradient_length,
+        )
 
     return peptide_rt
