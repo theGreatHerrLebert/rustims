69b0922f1d26924fd1917174b7f3bad7c9214cea
zix-xiao <cory.zixuan.xiao@outlook.com>
2025-05-14 15:43:22 +0200

    add access to property prediction models hosted on koina

---
+++ imspy/imspy/algorithm/koina_models/access_models.py
@@ -0,0 +1,41 @@
+# If you get a ModuleNotFound error install koinapy with `pip install koinapy`.
+from koinapy import Koina
+import numpy as np
+import pandas as pd
+
+
+class ModelFromKoina:
+    """
+    Class to access peptide property prediction models hosted on Koina. Available properties include:
+    - Fragment Intensity
+    - Retention Time
+    - Collision Cross Section
+    - Cross-Linking Fragment Intensity
+    - Flability
+    """
+
+    def __init__(self, model_name: str, host: str = "koina.wilhelmlab.org:443"):
+        """
+        Args:
+            model_name: Name of the Koina model. See all available models at https://koina.wilhelmlab.org/docs
+            host: Host of the Koina server.
+        """
+        self.model = Koina(model_name, host)
+        self.model_name = model_name
+
+    def predict(self, inputs: pd.DataFrame) -> pd.DataFrame:
+        """
+        Predict the output of the model.
+        Args:
+            inputs: Input data for the model. requires a DataFrame with the following columns:
+                - 'peptide_sequences': Peptide sequence with unimod format, always required.
+                - 'precursor_charges': Precursor charge state. Required for some models.
+                - 'collision_energies': Collision energy. Required for some models.
+                - 'fragmentation_types': Fragmentation type. Required for some models.
+                To see what inputs are required for a specific model, run `model.model_inputs`
+                For requirements on input sequence length or modifications, see the Koina documentation.
+        Returns:
+            pd.DataFrame: Output data from the model.
+        """
+        predictions = self.model.predict(inputs)
+        return predictions
