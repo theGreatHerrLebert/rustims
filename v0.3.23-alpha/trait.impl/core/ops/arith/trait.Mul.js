80bd29f61f0ef014b30b3f9acec55a97bc3968e5
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2025-08-05 11:33:54 +0000

    deploy: ba82d93a0c3f6b89451d691deb99af4431d7fcca

---
+++ v0.3.23-alpha/trait.impl/core/ops/arith/trait.Mul.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mscore",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.f64.html\">f64</a>&gt; for <a class=\"struct\" href=\"mscore/data/spectrum/struct.MzSpectrum.html\" title=\"struct mscore::data::spectrum::MzSpectrum\">MzSpectrum</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.f64.html\">f64</a>&gt; for <a class=\"struct\" href=\"mscore/simulation/annotation/struct.MzSpectrumAnnotated.html\" title=\"struct mscore::simulation::annotation::MzSpectrumAnnotated\">MzSpectrumAnnotated</a>"]]],["rustms",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.f64.html\">f64</a>&gt; for <a class=\"struct\" href=\"rustms/ms/spectrum/struct.MzSpectrum.html\" title=\"struct rustms::ms::spectrum::MzSpectrum\">MzSpectrum</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[841,402]}
