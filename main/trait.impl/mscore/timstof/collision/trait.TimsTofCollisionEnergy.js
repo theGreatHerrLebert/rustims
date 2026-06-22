17cdd5d698de6e20f9cec3b533baf28969b2f7ee
theGreatHerrLebert <theGreatHerrLebert@users.noreply.github.com>
2026-06-22 06:51:29 +0000

    deploy: dc8d233736db012216fda784e867931286a39992

--- main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
+++ main/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["mscore",[]],["rustdf",[["impl <a class=\"trait\" href=\"mscore/timstof/collision/trait.TimsTofCollisionEnergy.html\" title=\"trait mscore::timstof::collision::TimsTofCollisionEnergy\">TimsTofCollisionEnergy</a> for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>",0],["impl <a class=\"trait\" href=\"mscore/timstof/collision/trait.TimsTofCollisionEnergy.html\" title=\"trait mscore::timstof::collision::TimsTofCollisionEnergy\">TimsTofCollisionEnergy</a> for <a class=\"struct\" href=\"rustdf/sim/lazy_builder/struct.TimsTofLazyFrameBuilderDIA.html\" title=\"struct rustdf::sim::lazy_builder::TimsTofLazyFrameBuilderDIA\">TimsTofLazyFrameBuilderDIA</a>",0]]]]);
+    const implementors = Object.fromEntries([["mscore",[]],["rustdf",[["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>",0],["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/lazy_builder/struct.TimsTofLazyFrameBuilderDIA.html\" title=\"struct rustdf::sim::lazy_builder::TimsTofLazyFrameBuilderDIA\">TimsTofLazyFrameBuilderDIA</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[13,793]}
+//{"start":59,"fragment_lengths":[13,477]}
